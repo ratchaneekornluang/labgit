8297f787f74af745d64e8f5c5c9eef62381bbfe3
Jet <jetzjamez@hotmail.com>
2017-11-09 12:24:20 +0700

    ADD msg sud yhoddd!

--- Test/src/Controller/LoginController.java
+++ Test/src/Controller/LoginController.java
@@ -4,5 +4,6 @@ package Controller;
 public class LoginController {
 		public static void main(String[] args) {
 			System.out.print("Hello fong");
+			System.out.println("Jet inw 555+ za za");
 		}
 }
